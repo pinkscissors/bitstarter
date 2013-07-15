a13350ade9e4b4d5b18e4119caf68437cdffb39c
pinkscissors <robwll@btinternet.com>
2013-07-15 21:11:09 +0000

    Errors corrected

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require ("fs");
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(fs.readFileSync("index.html", "utf-8");
+var buf = new Buffer(fs.readFileSync("index.html", "utf-8"));
 
 
 app.get('/', function(request, response) {
